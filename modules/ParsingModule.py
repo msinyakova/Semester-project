4775b6583f8717ff535d64fc87c13c3949ef7bbd
Maxim <maksim090897@mail.ru>
2019-04-30 00:34:23 +0300

    Tests

---
+++ modules/ParsingModule.py
@@ -0,0 +1,34 @@
+import re
+
+CONST_ALPHABET = ['a','b','c','*']
+
+# str rule
+# return lst
+def ruleStrToList(rule):
+    flag = True
+    res = re.match('(.*)\\|->'+'(.*)$', rule, re.MULTILINE)
+    if (isinstance(res, type(None))):
+        flag = False
+        res = re.match('(.*)->'+'(.*)$', rule, re.MULTILINE)
+    if (isinstance(res, type(None))):
+        return []
+    return [res.group(1), res.group(2), flag]
+
+
+# lst rule
+# return lst
+def validateRuleList(rule):
+    i = 0
+    for k in rule[0]:
+        if k not in CONST_ALPHABET:
+            return [i,k]
+        i += 1
+    if rule[2]:
+        i += 3
+    else:
+        i += 2
+    for k in rule[1]:
+        if k not in CONST_ALPHABET:
+            return [i,k]
+        i += 1
+    return []
